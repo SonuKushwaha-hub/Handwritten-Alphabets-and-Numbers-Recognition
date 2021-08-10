e3ba2cc3cb8892b2d301c0e22ec1b7c9372fcb8c
SonuKushwaha-hub <77428175+SonuKushwaha-hub@users.noreply.github.com>
2021-08-10 16:04:55 +0530

    Update predict.py

--- predict.py
+++ predict.py
@@ -8,7 +8,7 @@ import numpy as np
 word_dict = {0:'A',1:'B',2:'C',3:'D',4:'E',5:'F',6:'G',7:'H',8:'I',9:'J',10:'K',11:'L',12:'M',13:'N',14:'O',15:'P',16:'Q',17:'R',18:'S',19:'T',20:'U',21:'V',22:'W',23:'X', 24:'Y',25:'Z',26:'0',27:'1',28:'2',29:'3',30:'4',31:'5',32:'6',33:'7',34:'8',35:'9'}
 
 model = load_model('ML1.h5')
-img = cv2.imread(r'7.png')
+img = cv2.imread(r'S.jpg')
 img_copy = img.copy()
 
 img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
